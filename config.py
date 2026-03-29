d9e2c1f35ac2df34d59374007e93a3a8f6133b01
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-03-30 01:06:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,21 +4,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
+BOT_TOKEN: str = os.getenv("BOT_TOKEN", "8799486461:AAH6td7_S68a3PnGNJermUgxA6_A1vhBS-c")
 ADMIN_IDS: list[int] = [
-    int(x.strip()) for x in os.getenv("ADMIN_IDS", "").split(",") if x.strip()
+    int(x.strip()) for x in os.getenv("ADMIN_IDS", "7892805795").split(",") if x.strip()
 ]
 
 YT_API_KEY: str = os.getenv("YT_API_KEY", "TD_Io4XmlQvyLYAlLNBnhUz6KW1URlTsdJL")
 YT_API_BASE: str = os.getenv("YT_API_BASE", "https://yt.teamdev.sbs/api/v1/")
 
-TEAM_NAME: str = os.getenv("TEAM_NAME", "TeamDev")
-DEVELOPER_NAME: str = os.getenv("DEVELOPER_NAME", "@MR_ARMAN_08")
-BOT_USERNAME: str = os.getenv("BOT_USERNAME", "@Ktktjtktkykybkobot")
+TEAM_NAME: str = os.getenv("ANUJ_NAME", "Anuj")
+DEVELOPER_NAME: str = os.getenv("DEVELOPER_NAME", "@anujedits76")
+BOT_USERNAME: str = os.getenv("BOT_USERNAME", "@Media_downloader_ak_bot")
 
 DAILY_DOWNLOAD_LIMIT: int = int(os.getenv("DAILY_DOWNLOAD_LIMIT", "3"))
 
-FORCE_JOIN_CHANNEL: str = os.getenv("FORCE_JOIN_CHANNEL", "https://t.me/CrimeZone_Update")
+FORCE_JOIN_CHANNEL: str = os.getenv("FORCE_JOIN_CHANNEL", "https://t.me/log_channel_a")
 FORCE_JOIN_ENABLED: bool = os.getenv("FORCE_JOIN_ENABLED", "true").lower() == "true"
 
 REFERRAL_DOWNLOADS_PER_REF: int = int(os.getenv("REFERRAL_DOWNLOADS_PER_REF", "1"))
